6d334f0efa966bfd98400c15661bae65fa2dc47b
Michael S <mlschott343@gmail.com>
2018-09-01 21:30:56 -0700

    Update 1
    Added more to main.py

--- main.py
+++ main.py
@@ -1,4 +1,21 @@
 #!/user/bin/env python
+import numpy as np
+
+
+class World:
+
+    def __init__0(self):
+        '''
+        Initialization function for the World
+        '''
+
+        self.x = 100
+        self.y = 100
+        self.food_count = 100
+
+        self.food_positions = 100*np.random.rand(self.food_count, 2)
+
+        print(self.food_positions)
 
 
 class Herbivore:
@@ -7,3 +24,6 @@ class Herbivore:
         '''
         Initialization Function
         '''
+
+
+world = World()
